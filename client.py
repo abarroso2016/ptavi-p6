78e37cd2d3c1f628728afb0c3d3431c2f6f72006
Andres <a.barroso.2016@alumnos.urjc.es>
2018-11-26 16:11:13 +0100

    Realizo primeras pruebas

--- client.py
+++ client.py
@@ -5,12 +5,20 @@ Programa cliente que abre un socket a un servidor
 """
 
 import socket
+import sys
 
 # Cliente UDP simple.
 
 # Dirección IP del servidor.
-SERVER = 'localhost'
-PORT = 6001
+try:
+    SERVER = 'localhost'
+    METHOD = sys.argv[1]
+    BASH = sys.argv[2].split(":")
+    SIP = BASH[0]
+    PORT = int(BASH[1])
+except IndexError:
+    print("Usage: python3 client.py method receiver@IP:SIPport")
+    sys.exit()
 
 # Contenido que vamos a enviar
 LINE = '¡Hola mundo!'
@@ -19,12 +27,20 @@ LINE = '¡Hola mundo!'
 with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as my_socket:
     my_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
     my_socket.connect((SERVER, PORT))
+	
+    if METHOD == 'INVITE':
+        LINE = "INVITE " + SIP + " SIP/2.0\r\n"
+        print("Enviando: " + LINE)
+        my_socket.send(bytes(LINE, 'utf-8') + b'\r\n')
+        data = my_socket.recv(1024)
+        recibido = data.decode('utf-8').split("\rn\rn")
+        if recibido[0] == "SIP/2.0 200 OK\r\n\r\n":
+            print(data.decode('utf-8'))
+            LINE = "ACK " + SIP + " SIP/2.0\r\n"
+            print("Enviando: " + LINE)
+            my_socket.send(bytes(LINE, 'utf-8') + b'\r\n')
 
-    print("Enviando: " + LINE)
-    my_socket.send(bytes(LINE, 'utf-8') + b'\r\n')
-    data = my_socket.recv(1024)
-
-    print('Recibido -- ', data.decode('utf-8'))
+    
     print("Terminando socket...")
 
 print("Fin.")
--- server.py
+++ server.py
@@ -14,18 +14,30 @@ class EchoHandler(socketserver.DatagramRequestHandler):
 
     def handle(self):
         # Escribe dirección y puerto del cliente (de tupla client_address)
-        self.wfile.write(b"Hemos recibido tu peticion")
-        while 1:
+        line = self.rfile.read()
+        print("El cliente nos manda " + line.decode('utf-8'))
+        prueba = line.decode('utf-8').split(" ")
+        METHOD = prueba[0]
+        if METHOD == "INVITE":
+            self.wfile.write(b"SIP/2.0 100 Trying\r\n\r\n")
+            self.wfile.write(b"SIP/2.0 180 Ringing\r\n\r\n")
+            self.wfile.write(b"SIP/2.0 200 OK\r\n\r\n")
+        
+        #self.wfile.write(b"Hemos recibido tu peticion")
+        #while 1:
             # Leyendo línea a línea lo que nos envía el cliente
-            line = self.rfile.read()
-            print("El cliente nos manda " + line.decode('utf-8'))
+            #line = self.rfile.read()
+           # print("El cliente nos manda " + line.decode('utf-8'))
 
             # Si no hay más líneas salimos del bucle infinito
-            if not line:
-                break
+           # if not line:
+               # break
 
 if __name__ == "__main__":
     # Creamos servidor de eco y escuchamos
     serv = socketserver.UDPServer(('', 6001), EchoHandler)
-    print("Lanzando servidor UDP de eco...")
-    serv.serve_forever()
+    print("Listening...")
+    try:
+        serv.serve_forever()
+    except KeyboardInterrupt:
+        print("Finalizado servidor")
